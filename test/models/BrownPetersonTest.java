767b65c0ff3e64e4cdc3fa2e0dc230e151ff2f30
peat2110 <pppppooo@hotmail.com>
2014-01-24 21:38:20 +0700

    modify file java

---
+++ test/models/BrownPetersonTest.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import java.util.Date;
+import static org.junit.Assert.*;
+import play.test.WithApplication;
+import static play.test.Helpers.*;
+import models.brownPeterson.*;
+import java.util.List;
+
+public class BrownPetersonTest extends WithApplication {
+	@Before
+	public void setUp() {
+		start(fakeApplication(inMemoryDatabase()));
+	}
+}
