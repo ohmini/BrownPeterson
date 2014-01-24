750e8acb254c6f6e244a5855e7fcf87c17d77594
sakgunner <sakgunner@gmail.com>
2014-01-24 20:22:35 +0700

    refactor code brownpeterson

---
+++ app/models/brownPeterson/ExperimentSchedule.java
@@ -0,0 +1,48 @@
+package models.brownPeterson;
+
+import play.db.ebean.*;
+import javax.persistence.*;
+import java.util.Date;
+import play.data.format.Formats;
+import java.util.List;
+
+@Entity
+public class ExperimentSchedule extends Model{
+	@Id
+	public long id;
+	public String name;
+	public int noOfTrial;
+	public Date startDate;
+	public Date expireDate;
+
+	public ExperimentSchedule() {
+		this.name = "Brown Peterson";
+		this.noOfTrial = 3;
+	}
+
+	public static ExperimentSchedule findById(int id) {
+		return find.byId(new Long(id));
+	}
+
+	public static ExperimentSchedule create(String name, int noOfTrial, Date startDate, Date expireDate) {
+		if(isCorrectDate(startDate, expireDate)) {
+			ExperimentSchedule experimentSchedule = new ExperimentSchedule();
+			experimentSchedule.name = name;
+			experimentSchedule.noOfTrial = noOfTrial;
+			experimentSchedule.startDate = startDate;
+			experimentSchedule.expireDate = expireDate;
+			return experimentSchedule;
+		}
+		return null;
+	}
+
+	private static boolean isCorrectDate(Date startDate, Date expireDate) {
+		return (startDate.before(expireDate) && (startDate.after(new Date()) || startDate.equals(new Date())));
+	}
+
+	public static List<ExperimentSchedule> findAll(){
+		return find.all();
+	}
+
+	public static Finder<Long,ExperimentSchedule> find = new Finder(Long.class,ExperimentSchedule.class);
+}
