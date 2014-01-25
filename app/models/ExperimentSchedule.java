5c8c05171d511480a33ebee76a585b06a05f4f0a
Ratthasorn Thassana <ohmzuu@gmail.com>
2014-01-25 07:50:11 +0700

    Trial can findInvolving by Experiment

--- app/models/ExperimentSchedule.java
+++ app/models/ExperimentSchedule.java
@@ -6,59 +6,29 @@ import java.util.Date;
 import play.data.format.Formats;
 import java.util.List;
 import com.avaje.ebean.*;
+import java.util.ArrayList;
 
 @Entity
 public class ExperimentSchedule extends Model{
 	@Id
 	public long id;
-	public String name;
-	public int noOfTrial;
+	public String name = "Blank Experiment";
+	public int noOfTrial = 3;
 	public Date startDate;
 	public Date expireDate;
+	public ExperimentType experimentType;
 
-	public ExperimentSchedule() {
-		this.name = "Brown Peterson";
-		this.noOfTrial = 3;
+	public ExperimentSchedule(String name, int noOfTrial, Date startDate, Date expireDate, ExperimentType experimentType) {
+		this.name = name;
+		this.noOfTrial = noOfTrial;
+		this.startDate = startDate;
+		this.expireDate = expireDate;
+		this.experimentType = experimentType;
 	}
 
-	public static List<ExperimentSchedule> getCurrentWorking() {
-		//List<ExperimentSchedule> experimentScheduleList = find.where().ge("startDate",new Date()).le("expireDate",new Date()).findList();
-		List<ExperimentSchedule> experimentScheduleList = findAll();
-		for (ExperimentSchedule e : experimentScheduleList){
-			if (!isWorkingDate(e.startDate,e.expireDate)){
-				experimentScheduleList.remove(e);
-			}
-		}
-		return experimentScheduleList;
-	}
-
-	public static ExperimentSchedule findById(int id) {
-		return find.byId(new Long(id));
-	}
-
-	public static ExperimentSchedule create(String name, int noOfTrial, Date startDate, Date expireDate) {
-		if(isCorrectDate(startDate, expireDate)) {
-			ExperimentSchedule experimentSchedule = new ExperimentSchedule();
-			experimentSchedule.name = name;
-			experimentSchedule.noOfTrial = noOfTrial;
-			experimentSchedule.startDate = startDate;
-			experimentSchedule.expireDate = expireDate;
-			return experimentSchedule;
-		}
-		return null;
-	}
-
-	private static boolean isCorrectDate(Date startDate, Date expireDate) {
-		return (startDate.before(expireDate) && (startDate.after(new Date()) || startDate.equals(new Date())));
-	}
-
-	private static boolean isWorkingDate(Date startDate, Date expireDate) {
-		return ((startDate.before(new Date()) || startDate.equals(new Date())) && (expireDate.after(new Date()) ||  expireDate.equals(new Date()) ) );
-	}
-
-
-	public static List<ExperimentSchedule> findAll(){
-		return find.all();
+	public static List<ExperimentSchedule> getAllWorkingExperiment() {
+		List<ExperimentSchedule> experiments = find.where().betweenProperties("startDate", "expireDate", new Date()).findList();
+		return experiments;
 	}
 
 	public static Finder<Long,ExperimentSchedule> find = new Finder(Long.class,ExperimentSchedule.class);
--- test/models/brownPetersonTest/TrialTest.java
+++ test/models/brownPetersonTest/TrialTest.java
@@ -4,6 +4,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import models.brownPeterson.*;
 import models.*;
+import java.util.Date;
 
 public class TrialTest extends WithApplication {
 	@Before
@@ -25,18 +26,39 @@ public class TrialTest extends WithApplication {
 		assertEquals(TrigramLanguage.ENGLISH, new Trial().trigramLanguage);
 	}
 
+	@Ignore
 	@Test
 	public void trialShouldBeCreate() {
-		new ExperimentSchedule().save();
-		ExperimentSchedule q_set = ExperimentSchedule.findById(1);
-		Trial trial = Trial.create(q_set);
-		assertEquals(q_set.id, trial.experimentSchedule.id);
+		fail();
 	}
 
 	@Test
 	public void trialShouldBeFindById() {
 		new Trial().save();
 		assertEquals(1, Trial.findById(1).id);
+	}
+
+	@Test
+	public void shoudBeFindByTrialInvolvingAndNotNull(){
+		Date date = new Date();
+		new ExperimentSchedule("Experiment 1", 5, new Date(date.getYear()-1, 0, 1), 
+			new Date(date.getYear()+1, 0, 31), ExperimentType.BROWNPETERSON).save();
+		ExperimentSchedule ex = ExperimentSchedule.find.byId(new Long(1));
+		Trial.create(ex).save();
+		Trial.create(ex).save();
+		Trial.create(ex).save();
+
+		assertEquals(3, Trial.findInvolving(ex).size());
+	}
+
+	@Test
+	public void shoudBeFindByTrialInvolvingAndReferCorrectly(){
+		Date date = new Date();
+		new ExperimentSchedule("Experiment 1", 5, new Date(date.getYear()-1, 0, 1), 
+			new Date(date.getYear()+1, 0, 31), ExperimentType.BROWNPETERSON).save();
+		ExperimentSchedule ex = ExperimentSchedule.find.byId(new Long(1));
+		Trial trial = Trial.create(ex);
+		assertEquals(trial.schedule.id, ex.id);
 	}
 
 	
